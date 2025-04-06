1bdb96d05c365f97e4a021b02d8cae433c0f5748
devguialmeida <guilhermebarboza007@gmail.com>
2025-04-05 23:06:42 -0300

    bin search

---
+++ src/buscaBinCEP.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include "../include/endereco.h"
+#include <string.h>
+
+
+int c = 0;
+
+int busca_bin(Endereco * e, FILE * f, char * cep, int inicio, int fim) 
+{
+
+  if (inicio > fim) {
+    return 0;
+  }  
+
+  int meio = (inicio + fim) / 2;
+
+
+
+  fseek(f, meio * sizeof(Endereco), SEEK_SET);
+  fread(e, sizeof(Endereco), 1 , f);
+  
+  c++;
+  
+  int cmp = strncmp(e->cep, cep, 8);
+
+  if (cmp == 0) {
+    return 1;
+  }
+  else if (cmp > 0) {
+   return busca_bin(e,f,cep,inicio,meio-1);
+  }
+  else {
+    return busca_bin(e,f,cep,meio+1, fim);
+  }
+}
+
+
+int main (int argc, char ** argv)
+{   
+  FILE * fPtr;
+  Endereco e;
+  int qt;
+
+
+  if (argc != 2) {
+    fprintf(stderr, "USO: %s [CEP]\n", argv[0]);
+    return 1;
+  }
+  
+  c = 0;
+
+  if((fPtr = fopen("data/cep_ordenado.dat", "rb")) == NULL) {
+    printf("Erro ao abrir o arquivo. O programa será abortado.");
+    return 1;
+  }
+
+  long int tam;
+  fseek(fPtr,0,SEEK_END);
+  tam = ftell(fPtr)/sizeof(Endereco);
+  rewind(fPtr);
+
+  int achei = busca_bin(&e, fPtr, argv[1],0 , tam);
+  if (achei == 0) {
+    printf("Não existe tal CEP!\n");
+    return 1;
+  } 
+  else {
+    printf("%.72s\n%.72s\n%.72s\n%.72s\n%.2s\n%.8s\n", e.logradouro, e.bairro, e.cidade, e.uf, e.sigla, e.cep);
+  }
+
+  printf("Total lido: %d\n", c);
+  fclose(fPtr);
+
+
+  return 0;
+}
---
+++ src/buscaCEP.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include "../include/endereco.h"
+
+//typedef struct _Endereco Endereco;
+
+// struct _Endereco {
+//     char logradouro[72];
+//     char bairro[72];
+//     char cidade [72];
+//     char uf[72];
+//     char sigla[2];
+//     char cep[8];
+//     char lixo[2]; // espaço + quebra de linha ao final de cada registro
+// };
+
+int main (int argc, char ** argv) {
+  
+    FILE * fPtr;
+    Endereco e;
+    int qt;
+    int c;
+
+    if (argc != 2)  {
+        fprintf(stderr, "USO: %s [CEP]", argv[0]);
+        return 1;
+    }
+
+    c = 0;
+
+    printf("Tamanho da Estrutura: %ld\n\n", sizeof(Endereco));
+    if((fPtr = fopen("data/cep_ordenado.dat","rb")) == NULL) {
+        printf("Erro ao abrir o arquivo. O programa será abortado.\n");
+        return 1;
+    }
+    qt = fread(&e, sizeof(Endereco), 1 ,fPtr);
+    while(qt > 0) 
+    {
+        c++;
+        e.logradouro[71] = '\0'; // torna ele null terminated?
+
+        if(strncmp(argv[1], e.cep, 8) == 0) {
+            printf("%.72s\n%.72s\n%.72s\n%.72s\n%.2s\n%.8s\n", e.logradouro, e.bairro, e.cidade, e.uf, e.sigla, e.cep);
+            break;
+        }
+        qt = fread(&e, sizeof(Endereco), 1 ,fPtr);
+    }
+    printf("Total lido: %d\n", c);
+    fclose(fPtr);
+}
