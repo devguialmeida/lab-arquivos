99569eaf3d3417b1a2430c79149e0006fe879fd2
devguialmeida <guilhermebarboza007@gmail.com>
2025-04-18 18:18:55 -0300

    No commit message

--- include/enderecoUtils.h
+++ include/enderecoUtils.h
@@ -1,6 +1,7 @@
 #include <string.h>
 #include "endereco.h"
-
+#ifndef __ENDERECO_UTILS__
+#define __ENDERECO_UTILS__
 int compara(const void *e1, const void *e2)
 {
 	return strncmp(((Endereco*)e1)->cep,((Endereco*)e2)->cep,8);
@@ -10,3 +11,4 @@ int comparaIndice(const void *e1, const void *e2)
 {
 	return strncmp(((IndiceCEP*)e1)->cep,((IndiceCEP*)e2)->cep,8);
 }
+#endif
